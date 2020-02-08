5b584b815f84606281b0df5e0c45e7513a83e9ec
Aashay Patel <patelol@bc.edu>
2020-02-08 15:42:26 -0500

    Completed 5.6

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Aashay Patel on 2/8/20.
+//  Copyright © 2020 Aashay Patel. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
