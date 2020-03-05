2f050b538b1c4525aa0dd3d05590e824c51b1cfc
Aashay Patel <patelol@bc.edu>
2020-03-05 10:03:01 -0600

    Completed 5.11

---
+++ ToDo List/ListTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ListTableViewCell.swift
+//  ToDo List
+//
+//  Created by Aashay Patel on 3/5/20.
+//  Copyright © 2020 Aashay Patel. All rights reserved.
+//
+
+import UIKit
+
+protocol ListTableViewCellDelegate: class {
+    func checkBoxToggle(sender: ListTableViewCell)
+}
+
+class ListTableViewCell: UITableViewCell {
+    
+    weak var delegate: ListTableViewCellDelegate?
+
+    @IBOutlet weak var checkBoxButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBAction func checkToggled(_ sender: UIButton) {
+        delegate?.checkBoxToggle(sender: self)
+    }
+}
